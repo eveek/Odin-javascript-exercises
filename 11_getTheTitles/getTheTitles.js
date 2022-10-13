abe468b3844fd16885908413173cdce86fc2c56a
Victor Enweoru <venweoru@gmail.com>
2022-10-13 11:46:50 +0100

    Complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const tiles = [];
+    for (const obj of array){
+        const tileItem = obj.title;
+        tiles.push(tileItem)
+    }
+    return tiles
 };
 
 // Do not edit below this line
