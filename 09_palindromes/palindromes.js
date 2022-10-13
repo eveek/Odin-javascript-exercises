8247f076a7137dd71b9f7e1f722ed70742a00fce
Victor Enweoru <venweoru@gmail.com>
2022-10-13 11:31:10 +0100

    Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const newString = string.toLowerCase().split("");
+    const filterStr = newString.filter(subString => {
+        if(subString === " " || subString === "," || subString === "!" || subString === "."){
+            return false
+        }else{return true}
+    })
+    const normStr = filterStr.join("") 
+    const reverseStr = filterStr.reverse().join("") 
+    return normStr === reverseStr ? true: false;
 };
 
 // Do not edit below this line
