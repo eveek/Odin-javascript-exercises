ed3b124400320b98c0376c89f7de60db52682795
Victor Enweoru <venweoru@gmail.com>
2022-10-13 11:33:06 +0100

    Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiply, number) => multiply * number);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number ** power
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	for(let i = 1; i <= number; i--){
+    console.log(i)
+  }
 };
 
 // Do not edit below this line
