ad027a3f337ba3afa0847f779e671748204d930e
Victor Enweoru <venweoru@gmail.com>
2022-10-13 11:25:00 +0100

    Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    const num = parseFloat(n)
+    if(num < 0) return "OOPS"
+    if(num === 1) num;
+    
+    
+    const result = [0, 1];
+    
+    for (let i = 2; i <= num; i++) {
+        result[i] = result[i - 2] + result[i - 1];
+    }
+    
+    return result[result.length - 1];
 };
 
 // Do not edit below this line
